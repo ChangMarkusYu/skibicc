50c74b65af42f69c0ccd96470f5dc0900b66e3e8
Chang Yu <marcus.yu.56@gmail.com>
2026-05-06 23:54:12 -0700

    Skeleton for building AST.

--- parser.c
+++ parser.c
@@ -25,20 +25,6 @@ static bool is_punctuator_token(token* tok, const char* expected) {
   return is_token_string_match(tok, expected);
 }
 
-static bool is_prefix_operator(token* tok) {
-  return is_punctuator_token(tok, "++") || is_punctuator_token(tok, "--");
-}
-
-static bool is_postfix_operator(token* tok) {
-  return is_punctuator_token(tok, "++") || is_punctuator_token(tok, "--");
-}
-
-static bool is_unary_operator(token* tok) {
-  return is_punctuator_token(tok, "&") || is_punctuator_token(tok, "*") ||
-         is_punctuator_token(tok, "+") || is_punctuator_token(tok, "-") ||
-         is_punctuator_token(tok, "~") || is_punctuator_token(tok, "!");
-}
-
 static bool is_keyword_token(token* tok, const char* expected) {
   if (tok->token_type != TK_KEYWRD) {
     return false;
@@ -94,55 +80,141 @@ static void consume_any_identifier(parser* parser) {
 }
 
 // Forward declarations.
-static void parse_unary_expression(parser*);
-static void parse_expression(parser*);
+static void parse_unary_expression(parser*, ast_expression*);
+static void parse_expression(parser*, ast_expression*);
 
 static void parse_type_name(parser* parser) {
   // TODO: Implement this.
 }
 
-static void parse_cast_expression(parser* parser) {
+static void parse_cast_expression(parser* parser, ast_expression* expression) {
   token* tok = peek_token(parser);
   if (is_punctuator_token(tok, "(")) {
     consume_token(parser);
     parse_type_name(parser);
     consume_punctuator(parser, ")");
   }
-  parse_unary_expression(parser);
+  parse_unary_expression(parser, expression);
 }
 
-static void parse_primary_expression(parser* parser) {
+static ast_node* create_ast_expression(void) {
+  ast_node* node = calloc(sizeof(ast_node), /*__size=*/1);
+  if (!node) {
+    error("FATAL: create_ast_expression(): calloc() failed.");
+  }
+  node->node_type = AST_EXPR;
+  ast_expression* expression = calloc(sizeof(ast_expression), /*__size=*/1);
+  if (!expression) {
+    error("FATAL: create_ast_expression(): calloc() failed.");
+  }
+  node->node.expression = expression;
+  return node;
+}
+
+static ast_node* create_ast_constant(token* tok) {
+  ast_node* node = calloc(sizeof(ast_node), /*__size=*/1);
+  if (!node) {
+    error("FATAL: create_ast_constant(): calloc() failed.");
+  }
+  node->node_type = AST_CONST;
+  ast_constant* constant = calloc(sizeof(ast_constant), /*__size=*/1);
+  if (!constant) {
+    error("FATAL: create_ast_constant(): calloc() failed.");
+  }
+  constant->tok = tok;
+  node->node.consant = constant;
+  return node;
+}
+
+static ast_node* create_ast_variable(token* tok) {
+  ast_node* node = calloc(sizeof(ast_node), /*__size=*/1);
+  if (!node) {
+    error("FATAL: create_ast_variable(): calloc() failed.");
+  }
+  node->node_type = AST_VAR;
+  ast_variable* variable = calloc(sizeof(ast_variable), /*__size=*/1);
+  if (!variable) {
+    error("FATAL: create_ast_variable(): calloc() failed.");
+  }
+  variable->tok = tok;
+  node->node.variable = variable;
+  return node;
+}
+
+static ast_node* parse_primary_expression(parser* parser,
+                                          ast_expression* expression) {
   token* tok = peek_token(parser);
   token_type tok_type = tok->token_type;
-  // identifier, constant or string literal.
-  if (tok_type == TK_ICONST || tok_type == TK_FCONST || tok_type == TK_STRLIT ||
-      tok_type == TK_IDENT) {
+  // Constant or string literal.
+  if (tok_type == TK_ICONST || tok_type == TK_FCONST || tok_type == TK_STRLIT) {
     consume_token(parser);
-    return;
+    return create_ast_constant(tok);
+  }
+  // Identifier.
+  if (tok_type == TK_IDENT) {
+    consume_token(parser);
+    return create_ast_variable(tok);
   }
 
   // ( expression )
   if (is_punctuator_token(tok, "(")) {
     consume_token(parser);
-    parse_expression(parser);
+    parse_expression(parser, expression);
     consume_punctuator(parser, ")");
-    return;
+    return NULL;
   }
   // TODO: implement generic selection parsing.
-
   error_tok(tok);
+  return NULL;
 }
 
-static void parse_postfix_expression(parser* parser) {
+static ast_operator* create_ast_operator(token* tok,
+                                         ast_operator_type op_type) {
+  ast_operator* op = malloc(sizeof(ast_operator));
+  if (!op) {
+    error("FATAL: create_ast_operator(): malloc() failed.");
+  }
+  op->tok = tok;
+  op->op_type = op_type;
+  return op;
+}
+
+static ast_expression* parse_postfix_operator(parser* parser,
+                                              ast_expression* expression) {
+  token* tok = peek_token(parser);
+  if (tok->token_type != TK_PUNCT) {
+    return NULL;
+  }
+
+  if (is_token_string_match(tok, "++")) {
+    expression->op = create_ast_operator(tok, OP_POSTINC);
+  } else if (is_token_string_match(tok, "--")) {
+    expression->op = create_ast_operator(tok, OP_POSTDEC);
+  }
+
+  if (expression->op) {
+    consume_token(parser);
+    ast_node* lhs = create_ast_expression();
+    expression->lhs = lhs;
+    return lhs->node.expression;
+  }
+  return NULL;
+}
+
+static void parse_postfix_expression(parser* parser,
+                                     ast_expression* expression) {
   // TODO: handle compound struct literal ( type-name ) { initializer-list }
 
-  parse_primary_expression(parser);
+  // TODO: Handle this.
+  parse_primary_expression(parser, expression);
+
+  ast_expression* cur = expression;
   while (has_token(parser)) {
     token* tok = peek_token(parser);
     // postfix-expression [ expression ]
     if (is_punctuator_token(tok, "[")) {
       consume_token(parser);
-      parse_expression(parser);
+      parse_expression(parser, expression);
       consume_punctuator(parser, "]");
       continue;
     }
@@ -155,7 +227,7 @@ static void parse_postfix_expression(parser* parser) {
         consume_token(parser);
         continue;
       }
-      parse_expression(parser);
+      parse_expression(parser, expression);
       consume_punctuator(parser, ")");
       continue;
     }
@@ -170,8 +242,9 @@ static void parse_postfix_expression(parser* parser) {
 
     // postfix-expression ++
     // postfix-expression --
-    if (is_postfix_operator(tok)) {
-      consume_token(parser);
+    ast_expression* res_exp = parse_postfix_operator(parser, cur);
+    if (res_exp) {
+      cur = res_exp;
       continue;
     }
     // No matching token - no more postfix expression to parse.
@@ -179,20 +252,72 @@ static void parse_postfix_expression(parser* parser) {
   }
 }
 
-static void parse_unary_expression(parser* parser) {
+static ast_expression* parse_prefix_operator(parser* parser,
+                                             ast_expression* expression) {
   token* tok = peek_token(parser);
-  if (is_prefix_operator(tok)) {
+  if (tok->token_type != TK_PUNCT) {
+    return NULL;
+  }
+
+  if (is_token_string_match(tok, "++")) {
+    expression->op = create_ast_operator(tok, OP_PREINC);
+  } else if (is_token_string_match(tok, "--")) {
+    expression->op = create_ast_operator(tok, OP_PREDEC);
+  }
+
+  if (expression->op) {
     consume_token(parser);
-    parse_unary_expression(parser);
+    ast_node* lhs = create_ast_expression();
+    expression->lhs = lhs;
+    return lhs->node.expression;
+  }
+  return NULL;
+}
+
+static ast_expression* parse_unary_operator(parser* parser,
+                                            ast_expression* expression) {
+  token* tok = peek_token(parser);
+  if (tok->token_type != TK_PUNCT) {
+    return NULL;
+  }
+
+  if (is_token_string_match(tok, "*")) {
+    expression->op = create_ast_operator(tok, OP_DEREF);
+  } else if (is_token_string_match(tok, "&")) {
+    expression->op = create_ast_operator(tok, OP_ADDROF);
+  } else if (is_token_string_match(tok, "+")) {
+    expression->op = create_ast_operator(tok, OP_POS);
+  } else if (is_token_string_match(tok, "-")) {
+    expression->op = create_ast_operator(tok, OP_NEG);
+  } else if (is_token_string_match(tok, "!")) {
+    expression->op = create_ast_operator(tok, OP_NOT);
+  } else if (is_token_string_match(tok, "~")) {
+    expression->op = create_ast_operator(tok, OP_BITNOT);
+  }
+
+  if (expression->op) {
+    consume_token(parser);
+    ast_node* lhs = create_ast_expression();
+    expression->lhs = lhs;
+    return lhs->node.expression;
+  }
+  return NULL;
+}
+
+static void parse_unary_expression(parser* parser, ast_expression* expression) {
+  ast_expression* res_exp = parse_prefix_operator(parser, expression);
+  if (res_exp) {
+    parse_unary_expression(parser, res_exp);
     return;
   }
 
-  if (is_unary_operator(tok)) {
-    consume_token(parser);
-    parse_cast_expression(parser);
+  res_exp = parse_unary_operator(parser, expression);
+  if (res_exp) {
+    parse_cast_expression(parser, res_exp);
     return;
   }
 
+  token* tok = peek_token(parser);
   if (is_keyword_token(tok, "sizeof")) {
     consume_token(parser);
     if (is_punctuator_token(tok, "(")) {
@@ -201,20 +326,24 @@ static void parse_unary_expression(parser* parser) {
       consume_punctuator(parser, ")");
       return;
     }
-    parse_unary_expression(parser);
+    parse_unary_expression(parser, expression);
     return;
   }
   // TODO: Handle _Alignof
 
-  parse_postfix_expression(parser);
+  parse_postfix_expression(parser, expression);
 }
 
-static void parse_expression(parser* parser) { consume_any_literal(parser); }
+static void parse_expression(parser* parser, ast_expression* expression) {
+  parse_unary_expression(parser, expression);
+}
 
 static void parse_statement(parser* parser) {
   consume_keyword(parser, "return");
 
-  parse_expression(parser);
+  ast_node ast;
+  memset(&ast, 0, sizeof(ast_node));
+  parse_expression(parser, ast.node.expression);
 
   consume_punctuator(parser, ";");
 }
--- parser.h
+++ parser.h
@@ -4,19 +4,64 @@
 #include "lexer.h"
 
 typedef enum ast_node_type {
-  AN_UNKNOWN,
-  AN_EXPR,
-  AN_LITERAL,
-  AN_STMNT,
-  AN_FUNCDEF,
-  AN_RETURN,
-  AN_SEMICOL,
+  AST_UNKNOWN,
+  AST_EXPR,
+  AST_VAR,
+  AST_CONST,
+  AST_STMNT,
+  AST_FUNCDEF,
+  AST_RETURN,
+  AST_SEMICOL,
 } ast_node_type;
 
 typedef struct ast_node ast_node;
 
-typedef struct ast_expression {
+typedef struct ast_constant {
+  // Must be TK_FCONST, TK_ICONST or TK_STRLIT
   token* tok;
+} ast_constant;
+
+typedef struct ast_variable {
+  // Must be TK_IDENT
+  token* tok;
+} ast_variable;
+
+typedef enum ast_operator_type {
+  // Postfix "++"
+  OP_POSTINC,
+  // Postfix "--"
+  OP_POSTDEC,
+  // Pointer dereference "*"
+  OP_DEREF,
+  // Address of "&"
+  OP_ADDROF,
+  // Unary plus "+"
+  OP_POS,
+  // Unary minus "-"
+  OP_NEG,
+  // Logical NOT "!"
+  OP_NOT,
+  // Bitwise NOT "~"
+  OP_BITNOT,
+  // Prefix "++"
+  OP_PREINC,
+  // Prefix "--"
+  OP_PREDEC,
+} ast_operator_type;
+
+typedef struct ast_operator {
+  ast_operator_type op_type;
+  // Must be TK_PUNCT
+  token* tok;
+} ast_operator;
+
+typedef struct ast_expression {
+  ast_operator* op;
+  // Must be `AST_EXPR`, `AST_CONST` or `AST_VAR`. Always populated.
+  struct ast_node* lhs;
+  // Must be `AST_EXPR`, `AST_CONST` or `AST_VAR`. Populated if `op` is not NULL
+  // and is a binary operator.
+  struct ast_node* rhs;
 } ast_expression;
 
 typedef struct ast_statement {
@@ -28,6 +73,8 @@ struct ast_node {
   union {
     ast_expression* expression;
     ast_statement* statement;
+    ast_constant* consant;
+    ast_variable* variable;
   } node;
 };
 
